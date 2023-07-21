0df58f4c3e75a7f906a77b3d27f73f40056c70bd
josh4got <125082025+josh4got@users.noreply.github.com>
2023-07-21 17:35:38 -0600

    init files

---
+++ dev-portfolio-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[80px] flex justify-between items-center px-4 bg-slate-600">
+        {/* <div>
+            <img src="" alt="Logo Image" />
+        </div> */}
+    </div>
+  )
+}
+
+export default Navbar
