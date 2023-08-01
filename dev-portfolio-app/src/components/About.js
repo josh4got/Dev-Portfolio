821c2bdb75c3bc64ac8378784bb0ae4e6b7e7a09
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-01 14:54:31 -0600

    created about component

---
+++ dev-portfolio-app/src/components/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div name='about' className='w-full h-screen bg-slate-600'>
+            {/* About Me */}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <h1 className='text-4xl sm:text-7xl text-white font-bold'>About Me</h1>
+                <p className ='py-5'>
+                I'm a full-stack web developer
+                with a keen eye for detail and an unwavering commitment to excellence, 
+                I harness my problem-solving aptitude and technical know-how to create seamless 
+                user experiences and optimize website performance.
+                </p>
+                <button className='rounded bg-white md:hidden'>
+                    Let's Connect
+                </button>
+                <p className ='py-5 hidden md:flex'>
+                My proficiency in effective communication 
+                allows me to collaborate seamlessly with cross-functional teams, ensuring tangible outcomes 
+                and surpassing expectations.
+                I'm open to embracing new opportunities and exploring fresh horizons where I can 
+                contribute my expertise and passion for creating digital solutions. If you're looking for a 
+                dynamic collaborator to make a real impact, let's connect and bring visions to life!
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default About;
