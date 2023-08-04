6733817ebf6415887e73dd3d951daeef10b420aa
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-04 10:13:23 -0600

    added image links

--- dev-portfolio-app/src/components/Projects.js
+++ dev-portfolio-app/src/components/Projects.js
@@ -1,4 +1,8 @@
 import React from "react";
+import CareConnect from "../assets/CareConnect.PNG";
+import DinnerDate from "../assets/Dinner Date.PNG";
+import ComingSoon from "../assets/coming-soon.jpg";
+import EmployeeTracker from "../assets/Employee Tracker.PNG";
 
 const Projects = () => {
   return (
@@ -7,46 +11,42 @@ const Projects = () => {
         <h1 className='text-4xl sm:text-7xl text-white font-bold'>Projects</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-5">
           {/* Project 1 */}
-          <div className="bg-white p-4">
+          <div className="p-4 rounded-lg">
             <h2 className="text-xl font-bold">Care Connect</h2>
             <div className="flex flex-col md:flex-row items-center">
-            <img src="../../assets/images/CareConnect.PNG" alt="Project 1" className="w-full md:w-1/2 p-2" />
-              <p className="py-2 md:w-1/2">
-              Care Connect is a full stack web application that enables users to manage employee information within an organization.
-              </p>
+              <a href="https://vast-beach-72570.herokuapp.com/">
+                <img src={CareConnect} alt="Care Connect" className="w-full md:w-1/2 p-2" />
+              </a>
             </div>
           </div>
 
           {/* Project 2 */}
-          <div className="bg-white p-4">
-            <h2 className="text-xl font-bold">Project 2</h2>
+          <div className="p-4 rounded-lg">
+            <h2 className="text-xl font-bold">Task Hive</h2>
             <div className="flex flex-col-reverse md:flex-row items-center">
-              <img src="dev-portfolio-app\src\assets\images\CareConnect.PNG" alt="Project 2" className="w-full md:w-1/2 p-2" />
-              <p className="py-2 md:w-1/2">
-                Coming Soon
-              </p>
+              <a href="placeholder">
+                <img src={ComingSoon} alt="Coming Soon" className="w-full md:w-1/2 p-2" />
+              </a>
             </div>
           </div>
 
           {/* Project 3 */}
-          <div className="bg-white p-4">
+          <div className="p-4 rounded-lg">
             <h2 className="text-xl font-bold">Dinner Date</h2>
             <div className="flex flex-col md:flex-row items-center">
-              <img src="project3.jpg" alt="Project 3" className="w-full md:w-1/2 p-2" />
-              <p className="py-2 md:w-1/2">
-              This web application can give recipe recommendations for each day of the week based on a user's preferences then lets users add them to their calendar.
-              </p>
+              <a href="https://josh4got.github.io/Dinner-Date/">
+                <img src={DinnerDate} alt="Dinner Date" className="w-full md:w-1/2 p-2" />
+              </a>
             </div>
           </div>
 
           {/* Project 4 */}
-          <div className="bg-white p-4">
-            <h2 className="text-xl font-bold">Emplyee Tracker</h2>
+          <div className="p-4 rounded-lg">
+            <h2 className="text-xl font-bold">Employee Tracker</h2>
             <div className="flex flex-col-reverse md:flex-row items-center">
-              <img src="../../" alt="Project 4" className="w-full md:w-1/2 p-2" />
-              <p className="py-2 md:w-1/2">
-              Employee Tracker is a CMS(content management system) application that adds,stores,and updates employee information in the database.
-              </p>
+              <a href="https://drive.google.com/file/d/1rkPG0zECUZWdH_6yvI5l1SMCmZkCpQh-/view">
+                <img src={EmployeeTracker} alt="Employee Tracker" className="w-full md:w-1/2 p-2" />
+              </a>
             </div>
           </div>
         </div>
